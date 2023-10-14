06408dd55ac166137e019184bb08a55383ae34c2
RandoVanikov <randovanikov@gmail.com>
2023-10-14 18:47:14 +0300

    week6

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿
+string[] snacks = { "sushi", "pizza", "burger", "salad", "wings", "chips", "fries", "pie" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+Console.WriteLine($"Tonight we eating {snacks[randomIndex]}");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿string hello = " täna on ilus päev!";
+
+int stringLenght = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+
+char firstLetter = hello[0];
+//Console.Write(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+firstLetter = hello[0];
+//Console.Write(firstLetter);
+
+Console.Write(hello);
+
+//int wordCounter = 1;
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//	//if (hello[i] == ' ')
+//	//{wordCounter++;}
+//	if (char.IsWhiteSpace(hello[i]))
+
+//	{
+//		wordCounter++;
+//    }
+
+//}
+
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
